7cfd5e9a8f9227201248ffffe177931c2cf934cf
SalinasPaulDev <salinaspaul.dev@gmail.com>
2024-01-16 20:14:58 -0600

    add id logo

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { Header } from './Header'
 import { Loader } from '../components/Loader/Loader'
+import Logo from '/LogoEmpresa.svg'
 export const Home = () => {
   return (
     <div className="w-full text-center">
@@ -15,10 +16,12 @@ export const Home = () => {
 
 
         <Link to={'/questions'}>
-            <button className='mt-8 md:mt-20 bg-blue-500 py-2 px-4 rounded-md font-semibold text-white m-auto text-xl'>
+            <button className='mt-8 md:mt-10 bg-blue-500 py-2 px-4 rounded-md font-semibold text-white m-auto text-xl'>
                 ¡Comenzemos!
             </button>
         </Link>
+
+        <img src={Logo} alt='id creativa logo' className='w-[200px] m-auto mt-12 opacity-20'/>
     </div>
   )
 }
