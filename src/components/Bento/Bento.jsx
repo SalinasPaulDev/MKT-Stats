769a4adc01042d8527ce14ae06282059d8cb06d9
SalinasPaulDev <salinaspaul.dev@gmail.com>
2024-01-16 22:22:21 -0600

    change images

--- src/components/Bento/Bento.jsx
+++ src/components/Bento/Bento.jsx
@@ -13,7 +13,7 @@ export const Bento = () => {
   return (
     <article className="
         w-full max-w-[1400px]
-        grid grid-cols-10 auto-rows-[15rem] gap-4
+        grid grid-cols-10 auto-rows-[17rem] gap-4
         mx-auto p-20
     ">
     <BentoItem className="col-span-10 md:col-span-6" title="Documentación" description="" image={DocumentationImage} percentege={getPercentage(answers)}/>
